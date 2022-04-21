411f0909fbfef24db7e523d0a0ad25d1359b56d1
Ubuntu <ubuntu@ip-172-31-41-46.ec2.internal>
2022-04-21 18:36:29 +0000

    Finished Guessing Game File

--- guessing.py
+++ guessing.py
@@ -11,20 +11,30 @@ def main():
     # This Function Runs the Program
 
     # Input
-    number_as_string = input("Enter The Number You're Guessing (0-9):")
+    counter = 0
     Hidden_Number = random.randint(0, 9)
 
     # Process & Output
-    try:
-        number_as_int = int(number_as_string)
-        if number_as_int == Hidden_Number:
-            print("You Were Correct! Hooray! You're Lucky!")
-        else:
-            print("You Were Incorrect! You Fool! You Blundered!")
-    except Exception:
-        print("Ok wise guy why don’t you enter a real number next time.")
-    finally:
-        print("\nDone.")
+    while True:
+        try:
+            number_as_string = input("Enter The Number You're Guessing (0-9):")
+            number_as_int = int(number_as_string)
+            counter = counter + 1
+            if number_as_int > Hidden_Number:
+                print(
+                    "You're number looking kinda big there buddy. Guess a smaller number"
+                )
+            elif number_as_int < Hidden_Number:
+                print("Ooooh that's TOO LOW. Guess BIGGER my friend.")
+            else:
+                print("DING DING DING! CORRECT! It took you {0} tries!".format(counter))
+                break
+        except Exception:
+            print("Are You Even Trying? ENTER AN INTEGER!!!.")
+        finally:
+            print("")
+
+    print("\nDone.")
 
 
 if __name__ == "__main__":
